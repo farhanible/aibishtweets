5a4b62e47888218ad2ce9586c0df045e83fbc38c
Farhanible <farhanible@gmail.com>
2016-04-06 23:48:14 -0400

    new commit

--- credentials.py
+++
@@ -1,5 +0,0 @@
-
-consumer_key = 'YsEyJOgr4h1FCfk5TZ6owXwyn'
-consumer_secret = 'Sx1FhqUyt8Xwm6zmafD1kbaADF9TGwDAqPfORlupYJ6H79aYgI'
-access_token = '716484419633213440-GmPMZxFTFdQpz6v8AyR0p1aCCtTo2Ta'
-access_secret =	'F7L1WEZi7ADLhIvJV3kA1Brag4jAuDZQi8rv3V1wVZpum'
